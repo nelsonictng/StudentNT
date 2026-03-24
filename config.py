d89c704c972ae7fad4ff8f96903b39102d0bc9e2
edunelsonit <nmintoh@gmail.com>
2026-03-24 16:06:49 +0100

    Secrets Update

--- config.py
+++ config.py
@@ -1,16 +1,26 @@
 import os
 from dotenv import load_dotenv
+import streamlit as st
 
 load_dotenv()
 
 class Settings:
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-    OPENAI_BASE_URL = os.getenv("OPENAI_BASE_URL")
-    OPENAI_MODEL = os.getenv("OPENAI_MODEL")
+    #OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    #OPENAI_BASE_URL = os.getenv("OPENAI_BASE_URL")
+    #OPENAI_MODEL = os.getenv("OPENAI_MODEL")
 
-    AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
-    AUTH0_CLIENT_ID = os.getenv("AUTH0_CLIENT_ID")
-    AUTH0_CLIENT_SECRET = os.getenv("AUTH0_CLIENT_SECRET")
-    AUTH0_CALLBACK_URL = os.getenv("AUTH0_CALLBACK_URL")
+    #AUTH0_DOMAIN = os.getenv("AUTH0_DOMAIN")
+    #AUTH0_CLIENT_ID = os.getenv("AUTH0_CLIENT_ID")
+    #AUTH0_CLIENT_SECRET = os.getenv("AUTH0_CLIENT_SECRET")
+    #AUTH0_CALLBACK_URL = os.getenv("AUTH0_CALLBACK_URL")
+
+    OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+    OPENAI_BASE_URL = st.secrets["OPENAI_BASE_URL"]
+    OPENAI_MODEL = st.secrets["OPENAI_MODEL"]
+
+    AUTH0_DOMAIN = st.secrets["AUTH0_DOMAIN"]
+    AUTH0_CLIENT_ID = st.secrets["AUTH0_CLIENT_ID"]
+    AUTH0_CLIENT_SECRET = st.secrets["AUTH0_CLIENT_SECRET"]
+    AUTH0_CALLBACK_URL = st.secrets["AUTH0_CALLBACK_URL"]
 
 settings = Settings()
